665737ca14f7282c5294c170384758ca2eaa35eb
karakRohan <karakrohan2005@gmail.com>
2026-01-04 02:02:46 +0530

    CalculateCreditPoint Changes

--- Backend/src/utils/calculateCreditPoint.js
+++ Backend/src/utils/calculateCreditPoint.js
@@ -1,6 +1,8 @@
 function calculateCredits(wasteType, quantity, qualityRating) {
   let baseRate = 2; // default points per kg
 
+
+// Start of changes and Comparisons and Calculations
   switch (wasteType) {
     case "plastic":
       baseRate = 3;
