6e7de8a21b6b0720e1b1fa98aabcb36596e53193
karakRohan <karakrohan2005@gmail.com>
2026-01-04 12:13:54 +0530

    change in api.js

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,7 +1,7 @@
 
-// const BASE_URL = "https://hacknitr-codenix.onrender.com/api/v1";
+const BASE_URL = "https://hacknitr-codenix.onrender.com/api/v1";
 // const BASE_URL = "https://enviromat.onrender.com/api/v1"
-const BASE_URL = "http://localhost:4000/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
