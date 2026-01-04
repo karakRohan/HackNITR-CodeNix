9b65294f55c2f3167f49ddc7fc1ffaa16646e716
Srinjoy <srinjoymahanti@gmail.com>
2026-01-04 12:27:26 +0530

    fix cors error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -140,19 +140,27 @@ const allowedOrigins = [
   "https://enviro-mat.vercel.app"
 ]
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-  })
-)
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error("Not allowed by CORS"))
+//       }
+//     },
+//     credentials: true,
+//   })
+// )
 
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://hack-nitr-code-nix.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(
 	fileUpload({
